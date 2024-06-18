ad32b8cb6c5cb4338e50b534db4d27669065511a
0Hoxy <tinyfinger@naver.com>
2024-06-18 10:38:56 +0900

    Github Context

---
+++ src/components/users/UserResult.jsx
@@ -0,0 +1,26 @@
+import { useContext, useEffect } from 'react';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
+
+function UserResult() {
+  const { users, loading, fetchUsers } = useContext(GithubContext);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []); //앱 시작시 실행됨
+
+  if (!loading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+}
+
+export default UserResult;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserResult from '../components/users/UserResult';
+
+function Home() {
+  return (
+    <>
+      <UserResult />
+    </>
+  );
+}
+
+export default Home;
